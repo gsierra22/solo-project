84e9537caea3cb52e28c4753b009e2896c2f634f
gsierra22 <giancarlosierra2011@hotmail.com>
2022-08-09 15:01:29 -0500

    route to players

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
+import { HeaderComponent } from './header/header.component';
 import { ScoresComponent } from './scores/scores.component';
 import { PlayerComponent } from './player/player.component';
+import { FrontComponent } from './front/front.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Player2Component } from './player2/player2.component';
 
 const routes: Routes = [
   {path: 'players', component: PlayerComponent},
-  {path: 'scores', component:ScoresComponent}
+  {path: 'scores', component:ScoresComponent},
+  {path: '', component:FrontComponent }
 ];
 
 @NgModule({
